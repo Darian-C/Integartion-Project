f772b70fa1ad4661226e6ce833cf57d062280433
Darian-C <46728231+Darian-C@users.noreply.github.com>
2019-04-11 15:09:03 -0400

    Add files via upload

---
+++ Practice/src/Practice.java
@@ -0,0 +1,24 @@
+
+public class Practice {
+	//Here are some examples of private fields,public getters 
+	//and setters, a constructor with and without parameters
+		public class Student{
+			private String name;
+			private int age;
+			public String getName() {
+				return name;
+			}
+			public void setName(String name) {
+				this.name = name;
+			}
+			public int getAge() {
+				return age;
+			}
+			public void setAge(int age) {
+				this.age = age;
+			} 
+		}
+	}
+
+
+
